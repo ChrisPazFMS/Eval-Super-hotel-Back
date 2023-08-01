2d2466b7d166cd96a7d2eb71af3cb581f2e3c637
totocriss <christian.pazmany@gmail.com>
2023-08-01 12:06:57 +0200

    Initialisation de l'API pour la base de données de Super-hôtel.com

---
+++ super_hotel_api/src/main/java/fms/superhotelapi/SuperHotelApiApplication.java
@@ -0,0 +1,13 @@
+package fms.superhotelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHotelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHotelApiApplication.class, args);
+	}
+
+}
---
+++ super_hotel_api/src/test/java/fms/superhotelapi/SuperHotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fms.superhotelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
